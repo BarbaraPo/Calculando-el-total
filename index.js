27b4c68538394f334477e6c60e56281b1d7034f6
Bárbara Urrutia <polett50@gmail.com>
2025-04-20 21:50:25 -0400

    por fin

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-precio = 400000
+ let precio = 400000;
+ let cantidad = 0
 
-precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio
 
+ const valorSpan =document.querySelector ('.valor-total');
+ const cantidadSpan= document.querySelector ('.cantidad');
+
+ const precioSpan = document.querySelector(".precio-inicial");
+ precioSpan.innerHTML = precio
+
+ function actualizarVista() {
+    cantidadSpan.innerHTML = cantidad
+    valorSpan.innerHTML = precio * cantidad
+ }
+
+ function aumentar () {
+    cantidad++;
+    actualizarVista ();
+ }
+
+ function disminuir () {
+    cantidad --; 
+    actualizarVista ();
+ }
